c669e31287aff38580f21d8d5800176847387c36
Cody Kimb <codykimbell@gmail.com>
2021-04-22 09:17:07 -0500

    data validation

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -18,7 +18,6 @@ const pizzaController = {
     },
   
     // get one pizza by id
-    // get one pizza by id
     getPizzaById({ params }, res) {
       Pizza.findOne({ _id: params.id })
         .populate({
@@ -46,15 +45,15 @@ const pizzaController = {
     },
     // update pizza by id
     updatePizza({ params, body }, res) {
-        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
-            .then(dbPizzaData => {
-            if (!dbPizzaData) {
-                res.status(404).json({ message: 'No pizza found with this id!' });
-                return;
-            }
-            res.json(dbPizzaData);
-            })
-            .catch(err => res.status(400).json(err));
+      Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbPizzaData => {
+          if (!dbPizzaData) {
+            res.status(404).json({ message: 'No pizza found with this id!' });
+            return;
+          }
+          res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
     },
     // delete pizza
     deletePizza({ params }, res) {
